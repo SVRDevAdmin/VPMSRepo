d1762ee45d03956101d9e223c455b8931f276d17
SVRTechKenny <kenny@svrtech.com.my>
2024-08-07 14:00:20 +0800

    check-in new script added

--- VPMS.Lib.Data/obj/Debug/net8.0/VPMS.Lib.Data.AssemblyInfo.cs
+++ VPMS.Lib.Data/obj/Debug/net8.0/VPMS.Lib.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4cb8c170b6b704d88f33cf76d39f645378eee7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea5d17f64e9883268ebab8565a762b27bf417923")]
 [assembly: System.Reflection.AssemblyProductAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
