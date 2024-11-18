1e80e0f96ab42bc960cdfb55bffb24d281e8d09f
SVRTechKenny <kenny@svrtech.com.my>
2024-11-18 11:55:12 +0800

    merged code

--- VPMS.Lib.Data/obj/Debug/net8.0/VPMS.Lib.Data.AssemblyInfo.cs
+++ VPMS.Lib.Data/obj/Debug/net8.0/VPMS.Lib.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14fb8c3cef19699bfdda94a692c4c699b8df7ee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdbd6fcefdac52bc00c04aa3a5e3e2a9c99cdd97")]
 [assembly: System.Reflection.AssemblyProductAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
