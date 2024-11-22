22075c8c849cb2c5188df8df1e717e00c60d296f
Muhammad Azwan Masri <azwan@svrtech.com.my>
2024-11-22 09:46:14 +0800

    pushing latest codes

--- VPMS.Lib.Data/obj/Debug/net8.0/VPMS.Lib.Data.AssemblyInfo.cs
+++ VPMS.Lib.Data/obj/Debug/net8.0/VPMS.Lib.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f45f3cc025a749379b9d6e226a8b8c935d7e28c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+496b01fadd11a96c2a3434ea3184ab9294b8a771")]
 [assembly: System.Reflection.AssemblyProductAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
