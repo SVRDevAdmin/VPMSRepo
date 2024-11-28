e498cedb98da4d5a96c2120e1e3b20defec74a9c
SVRTechKenny <kenny@svrtech.com.my>
2024-11-28 14:13:33 +0800

    check-in favicon changes

--- VPMS.Lib.Data/obj/Debug/net8.0/VPMS.Lib.Data.AssemblyInfo.cs
+++ VPMS.Lib.Data/obj/Debug/net8.0/VPMS.Lib.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c8e912baa10f70bfa3a26cfc6dc09d5bad4647a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3540440eb23d5b4e8bfa9967dabab208e8d1a5ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VPMS.Lib.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
