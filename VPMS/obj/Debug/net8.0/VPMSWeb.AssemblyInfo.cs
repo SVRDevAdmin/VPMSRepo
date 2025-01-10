a3431550f8775e217c62e0ae76c65b61a18d0343
SVRTechKenny <kenny@svrtech.com.my>
2025-01-10 15:22:42 +0800

    check-in latest code

--- VPMS/obj/Debug/net8.0/VPMSWeb.AssemblyInfo.cs
+++ VPMS/obj/Debug/net8.0/VPMSWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VPMSWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de41b3aee7ae52263aa74892ce39918a2f66393")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9062672b091edffba7cf400a3caa17bf2110b161")]
 [assembly: System.Reflection.AssemblyProductAttribute("VPMSWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VPMSWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
